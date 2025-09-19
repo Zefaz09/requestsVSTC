917c03a9160fb2a8bbf6ed5a530965456952a432
Wacław <waclaw@MacBook-Air--Waclaw.local>
2025-09-19 21:16:03 +0300

    19.09.2025

--- bot_server.py
+++ bot_server.py
@@ -1,17 +1,46 @@
-from fastapi import FastAPI, Request
 from private import botToken
-import threading
 import telebot
-import requests
+from telebot import types
 import pymysql
-
+from private import connection, cursor
 
 bot = telebot.TeleBot(botToken)
-app = FastAPI()
+
+
+keyboard = types.InlineKeyboardMarkup(row_width = 1)
+kb = types.InlineKeyboardButton
+keyboard.add(kb(text="Для студентов", callback_data="students"), kb(text="Для преподавателей", callback_data="teachers"))
+
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет!")
+    # menu = bot.send_message(message.chat.id, "Добро пожаловать в бота для удобного просмотра рассписания УО\"ВГТК\"", reply_markup=keyboard)
+    cursor.execute("SELECT * FROM users WHERE chat_id = %s", [message.chat.id])
+    username = cursor.fetchone()
+    if not username:
+        cursor.execute("INSERT INTO users (chat_id, username) VALUES (%s, %s)", [message.chat.id, message.from_user.username])
+        connection.commit()
+    else:
+        bot.send_message(message.chat.id, f"Привет {username['username']}")
+        
+
+
+
+# @bot.callback_query_handler(func=lambda call: 1)
+# def callback_inline(call):
+#     if call.message:
+#         bot.answer_callback_query(call.id)
+#         markup = types.InlineKeyboardMarkup()
+#         markup.add(types.InlineKeyboardButton("Студенты", callback_data="students"))
+#         markup.add(types.InlineKeyboardButton("Преподаватели", callback_data="teachers"))
+
+#         if call.data == "students":
+#             bot.edit_message_text("Рассписание для студентов", chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
+#         elif call.data == "teachers":
+#             bot.edit_message_text("Рассписание для преподавателей", chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
+
+
 
 print("Бот запущен")
 bot.polling(none_stop=1)
--- parse.py
+++ parse.py
@@ -1,39 +1,62 @@
 import requests
 from bs4 import BeautifulSoup
+import zlib
+from private import connection, cursor
 
-def getSchedule(url: str, response):
-    # try:
-        response.encoding = "utf-8"
-        if response.status_code == 200:
-            html = response.text
+def getSchedule(html) -> list[list[str]]:
+    
+    soup = BeautifulSoup(zlib.decompress(html), 'html.parser')
+    table = soup.find('table')
+    rows = table.find_all('tr')
 
-            soup = BeautifulSoup(html, 'html.parser', from_encoding="utf-8")
-            table = soup.find('table')
-            rows = table.find_all('tr')
+    matrix = []
+    y = 0
 
-            matrix = []
+    for row in rows:
+        cells = row.find_all('td')
+        x = 0
 
-            for row in rows:
-                cells = row.find_all(['td', 'th'])
-                row_data = []
-                for cell in cells:
-                    # rowspan = int(cell.get('rowspan', 1))
-                    colspan = int(cell.get('colspan', 1))
-                    for _ in range(colspan):
-                        row_data.append(cell)
-                matrix.append(row_data)
+        for cell in cells:
+            rowspan = int(cell.get('rowspan', 1))
+            colspan = int(cell.get('colspan', 1))
 
-            return matrix
-    # except Exception as e:
-    #     print(f"Ошибка: {e}")
-    #     return []
+            while y < len(matrix) and x < len(matrix[y]) and matrix[y][x]:
+                x += 1
 
-# def getDate():
-#     matrix = getSchedule("https://www.vgtk.by/schedule/lessons/day-tomorrow.php")
-#     year = matrix[0][0].get_text().split()[-1]
-#     months
-#     return date
+            for dy in range(rowspan):
+                rowY = y + dy
+                while len(matrix) <= rowY:
+                    matrix.append([])
+                while len(matrix[rowY]) <= x + colspan - 1:
+                    matrix[rowY].append(None)
 
-# print(getDate())
+                for dx in range(colspan):
+                    matrix[rowY][x + dx] = cell
+
+            x += colspan
+        y += 1
+    return matrix
+
+def getScheduleGroup(group: str) -> list:
+    cursor.execute("SELECT schedule FROM PageData")
+    schedule = cursor.fetchone()
+    result = []
+    if schedule:
+        page = getSchedule(schedule['schedule'])
+        for row in range(len(page)):
+            for cell in range(len(page[row])):
+                if page[row][cell].text == group:
+                    for i in page[row: row + 11]:
+                        result.append(i[cell].text)
+    return result    
+print(getScheduleGroup("ИТ-21"))                
+
+
+                
+    
+    
+
+
+   
 
 
